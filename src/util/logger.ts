bfabb51d8840236f4389b1d047b5e08c64ee678d
Florian Mold <florian.mold@live.at>
2019-07-16 10:17:51 +0200

    [TASK] configure logger #1

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -6,25 +6,39 @@ const logDir = "logs/";
 const options: LoggerOptions = {
   level: process.env.LOG_LEVEL || "error",
 
-
   transports: [
     new transports.Console({
-      level: process.env.NODE_ENV === "production" ? "error" : "debug"
+      level: process.env.NODE_ENV === "production" ? "error" : "debug",
+      colorize: "all",
+      prettyPrint: true,
+      showLevel: true,
+      label: "Serious Game Backend",
+      handleExceptions: true
     }),
     new transports.DailyRotateFile({
-        frequency: "24h",
-        filename: logDir + "info-%DATE%.log",
-        maxsize: "20m",
-        level: "debug",
-        json: true,
-        showLevel: true,
-        label: "Serious Game Backend",
+      frequency: "24h",
+      filename: "info-%DATE%.log",
+      maxsize: "20m",
+      level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      json: true,
+      showLevel: true,
+      dirname: logDir,
+      label: "Serious Game Backend"
+    }),
+    new transports.File({
+      json: true,
+      colorize: "all",
+      prettyPrint: true,
+      label: "Serious Game Backend",
+      showLevel: true,
+      level: "error",
+      dirname: logDir,
+      filename: "error.log"
     })
   ],
   exceptionHandlers: [
     new transports.File({ filename: logDir + "exceptions.log" })
   ]
-
 };
 
 const logger = new Logger(options);
