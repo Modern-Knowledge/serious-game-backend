c70fd5648bd93fa2f74759740baa3e4bdbf69823
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-17 21:48:50 +0200

    [TASK] call registration database facade

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { Request, Response } from "express";
+import {TherapistFacade} from "../db/entity/user/TherapistFacade";
+import { Therapist } from '../lib/models/Therapist';
+
 
 const router = express.Router();
 
@@ -7,7 +10,10 @@ router.get("/", async (req: Request, res: Response) => {
   res.jsonp("RegisterController");
 });
 router.post("/", async (req: Request, res: Response) => {
-  res.jsonp("RegisterController");
+  const { body: { _email, _forename, _lastname, _password } } = req;
+  const therapistFacade = new TherapistFacade();
+  const response = await therapistFacade.insertTherapist(new Therapist(_email, _password, _forename, _lastname));
+  res.jsonp(response)
 });
 
 export default router;
