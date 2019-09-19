7271a7b97808f6b30ff540cc63c459eb82ccd7fb
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-19 17:48:00 +0200

    [TASK] adjust error response in login controller

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcryptjs";
-import { UserFacade } from '../db/entity/user/UserFacade';
-import logger from '../util/logger';
+import { UserFacade } from "../db/entity/user/UserFacade";
+import logger from "../util/logger";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -16,18 +16,24 @@ router.get("/logout", (req: Request, res: Response) => {
 
 router.post("/login", async (req: Request, res: Response) => {
   const userFacade = new UserFacade();
-  const { body: {email, password} } = req;
+  const {
+    body: { email, password }
+  } = req;
   const users = await userFacade.getUsers();
 
   const user = users.find(user => user.email === email);
-  if (!user) return res.status(404).send('Invalid credentials.');
+  if (!user) return res.status(401).send("Invalid credentials.");
 
   const valid = bcrypt.compareSync(password, user.password);
-  if (!valid) return res.status(401).send({ auth: false, token: null });
+  if (!valid) return res.status(401).send("Invalid credentials.");
 
-  const token = jwt.sign({ id: user.id, email: user.email }, process.env.SECRET_KEY, {
-    expiresIn: 3600 // expires in 1 hour
-  });
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: 3600 // expires in 1 hour
+    }
+  );
 
   return res.status(200).send({ auth: true, token: token });
 });
