f373513b0e94f4d3f40d4b3bcefaff86f43afc7e
Florian Mold <florian.mold@live.at>
2019-09-19 18:26:02 +0200

    [TASK] generic methods for querying database

--- src/db/entity/EntityFacade.ts
+++ src/db/entity/EntityFacade.ts
@@ -1,16 +1,47 @@
-import { BaseFacade } from "../BaseFacade";
-import { AbstractModel } from "../../lib/models/AbstractModel";
+import {BaseFacade} from "../BaseFacade";
+import {AbstractModel} from "../../lib/models/AbstractModel";
+import {SQLAttributes} from "../sql/SQLAttributes";
+import {FilterAttribute} from "../filter/FilterAttribute";
+import {SQLComparisonOperator} from "../sql/SQLComparisonOperator";
 
 /**
  * base facade for entities
  */
 export abstract class EntityFacade<EntityType extends AbstractModel> extends BaseFacade<EntityType> {
 
+    /**
+     * @param tableName
+     * @param tableAlias
+     */
+    protected constructor(tableName: string, tableAlias: string) {
+        super(tableName, tableAlias);
+    }
+
   /**
-   * @param tableName
-   * @param tableAlias
+   * returns entity by id
+   * @param id
+   * @param excludedSQLAttributes
    */
-  protected constructor(tableName: string, tableAlias: string) {
-    super(tableName, tableAlias);
-  }
+    public async getById(id: number, excludedSQLAttributes?: string[]): Promise<EntityType> {
+        const attributes: SQLAttributes = this.getSQLAttributes(excludedSQLAttributes);
+        const filter = this._filter;
+        filter.addFilterAttribute(new FilterAttribute("id", id, SQLComparisonOperator.EQUAL));
+
+        const result: EntityType[] = await this.select(attributes, this.getJoins());
+
+        if(result.length > 0) {
+          return result[0];
+        }
+
+        throw new Error("More than 1 row returned");
+    }
+
+    /**
+     * returns all entities with specified filter
+     * @param excludedSQLAttributes
+     */
+    public async get(excludedSQLAttributes?: string[]): Promise<EntityType[]> {
+      const attributes: SQLAttributes = this.getSQLAttributes(excludedSQLAttributes);
+      return this.select(attributes, this.getJoins());
+    }
 }
