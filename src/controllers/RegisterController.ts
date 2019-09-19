bd4f58985c8e05fa4939901be1172f2ed5a271b5
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-19 17:48:12 +0200

    [TASK] send token on registration

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import { Request, Response } from "express";
-import {TherapistFacade} from "../db/entity/user/TherapistFacade";
-import { Therapist } from '../lib/models/Therapist';
-import { Status } from '../lib/enums/Status';
+import { TherapistFacade } from "../db/entity/user/TherapistFacade";
+import { Therapist } from "../lib/models/Therapist";
+import { Status } from "../lib/enums/Status";
 import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  const { body: {_email, _password, _forename, _lastname} } = req;
+  const {
+    body: { _email, _password, _forename, _lastname }
+  } = req;
   const therapistFacade = new TherapistFacade();
   const therapist = new Therapist();
   therapist.email = _email;
@@ -17,7 +20,14 @@ router.post("/", async (req: Request, res: Response) => {
   therapist.lastname = _lastname;
   therapist.status = Status.ACTIVE;
   const response = await therapistFacade.insertTherapist(therapist);
-  res.status(201).jsonp(response);
+  const token = jwt.sign(
+    { id: response.id, email: response.email },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: 3600 // expires in 1 hour
+    }
+  );
+  res.status(201).jsonp({ auth: true, token: token, user: response });
 });
 
 export default router;
