f2136458d0148eaacaa5b08b679a3b0c0915bcbd
Florian Mold <florian.mold@live.at>
2019-09-20 11:26:22 +0200

    [FEATURE] windows build script

---
+++ windows-build.bat
@@ -0,0 +1,17 @@
+@echo off
+SETLOCAL
+
+echo delete lib folder
+if exist src\lib rmdir /s /q src\lib
+
+echo create lib folder
+mkdir src\lib
+
+echo copy lib folder
+robocopy "../serious-game-library/src" "src/lib" /E >nul
+
+echo delete docs folder
+if exist docs rmdir /s /q docs
+
+echo build docs
+typedoc & npm install && npm run watch
