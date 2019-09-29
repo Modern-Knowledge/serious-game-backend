e8924a9a41e231c40bfce75974af0d36ff2180fc
Florian Mold <florian.mold@live.at>
2019-09-29 10:41:14 +0200

    [TASK] mail-template after password was resetted

---
+++ src/mail-texts/passwordResettet.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 Florian Mold
+ * All rights reserved.
+ */
+
+import { SmtpMessage } from "./SmtpMessage";
+
+/**
+ * Template for sending the user an E-Mail after the password was resettet
+ * Params (3):
+ * - ::name:: name of the recipient
+ * - ::date_time_resetted:: datetime when the reset has taken place
+ * - ::support_mail:: support mail of the application
+ */
+const passwordResettet = new SmtpMessage();
+
+passwordResettet.subject = "Ihr Passwort wurde zurückgesetzt!";
+
+passwordResettet.html = `
+<h2>Sehr geehrte/r ::name::</h2>
+<p>Ihr Password wurde am ::date_time_resetted:: zurückgesetzt.</p>
+<p><b>Wenn Sie das nicht waren</b>, dann können Sie diese E-Mail ignorieren.</p>
+<p><b>Wenn Sie die Änderung nicht beantragt haben</b>, dann wenden Sie sich an den Support.</p>
+<p><b>Kontaktieren Sie uns!:</b> ::support_mail::</p>
+`;
+
+passwordResettet.text = `
+Sehr geehrte/r ::name::
+
+Ihr Password wurde am ::date_time_resetted:: zurückgesetzt.
+
+Wenn Sie das nicht waren, dann können Sie diese E-Mail ignorieren.
+Wenn Sie die Änderung nicht beantragt haben, dann wenden Sie sich an den Support.
+
+Kontaktieren Sie uns: ::support_mail::
+`;
+
+export { passwordResettet };
