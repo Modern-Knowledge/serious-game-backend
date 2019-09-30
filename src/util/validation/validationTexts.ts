fc40305a9d03db09ca813acd739a07bd5434dd2c
Florian Mold <florian.mold@live.at>
2019-09-30 12:18:44 +0200

    [FEATURE] provide localised error messages

---
+++ src/util/validation/validationTexts.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Florian Mold
+ * All rights reserved.
+ */
+export const validationTexts = {
+    email: {
+        invalid: "Keine gültige E-Mail übergeben!"
+    },
+    password: {
+        invalid: "Kein gültiges Passwort übergeben!",
+    },
+    token: {
+        invalid: "Kein gültiges Token übergeben!"
+    }
+
+};
