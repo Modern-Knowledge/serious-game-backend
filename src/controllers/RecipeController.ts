d8112e3ba28de01dc13788d1f4f9cb10edcb6b51
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-02 23:29:36 +0200

    [TASK] use HttpResponse in recipe controller

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -8,12 +8,38 @@ const router = express.Router();
 router.get("/", async (req: Request, res: Response) => {
   const recipeFacade = new RecipeCompositeFacade();
   const recipes = await recipeFacade.get();
-  res.jsonp(recipes);
+  if(!recipes.length){
+    res.json(new HttpResponse(HttpResponseStatus.FAIL,
+      undefined,
+      [
+          new HttpResponseMessage(HttpResponseMessageSeverity.DANGER, `Es konnten keine Rezepte gefunden werden.`)
+      ]
+    ));
+  }
+  return res.status(200).json(new HttpResponse(HttpResponseStatus.SUCCESS,
+    recipes,
+    [
+        new HttpResponseMessage(HttpResponseMessageSeverity.SUCCESS, null)
+    ]
+  ));
 });
 router.get("/:id", async (req: Request, res: Response) => {
   const recipeFacade = new RecipeCompositeFacade();
   const recipe = await recipeFacade.getById(req.params.id);
-  res.jsonp(recipe);
+  if(!recipe){
+    res.json(new HttpResponse(HttpResponseStatus.FAIL,
+      undefined,
+      [
+          new HttpResponseMessage(HttpResponseMessageSeverity.DANGER, `Das Rezept konnte nicht gefunden werden.`)
+      ]
+    ));
+  }
+  return res.status(201).json(new HttpResponse(HttpResponseStatus.SUCCESS,
+    recipe,
+    [
+        new HttpResponseMessage(HttpResponseMessageSeverity.SUCCESS, null)
+    ]
+  ));
 });
 
 export default router;
