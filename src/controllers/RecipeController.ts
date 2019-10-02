3c7b905bcd09f28e946e41df2d7fbb730fb3883f
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-02 22:12:28 +0200

    [TASK] use recipecompositefacade

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import { Request, Response } from "express";
-import { RecipeFacade } from "../db/entity/kitchen/RecipeFacade";
-import logger from "../util/logger";
+import { RecipeCompositeFacade } from '../db/composite/RecipeCompositeFacade';
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const recipeFacade = new RecipeFacade();
+  const recipeFacade = new RecipeCompositeFacade();
   const recipes = await recipeFacade.get();
   res.jsonp(recipes);
 });
 router.get("/:id", async (req: Request, res: Response) => {
-  const recipeFacade = new RecipeFacade();
+  const recipeFacade = new RecipeCompositeFacade();
   const recipe = await recipeFacade.getById(req.params.id);
   res.jsonp(recipe);
 });
