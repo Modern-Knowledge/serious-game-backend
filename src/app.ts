b642993f725b0fc4aed41717362561980ba969a4
Florian Mold <florian.mold@live.at>
2019-10-03 20:02:21 +0200

    [FEATURE] init statistics controller

--- src/app.ts
+++ src/app.ts
@@ -83,6 +83,7 @@ import PasswordResetController from "./controllers/PasswordResetController";
 import RecipeController from "./controllers/RecipeController";
 import WordController from "./controllers/WordController";
 import SessionController from "./controllers/SessionController";
+import StatisticController from "./controllers/StatisticController";
 
 /**
  * measure response time
@@ -108,7 +109,7 @@ app.use("/password", PasswordResetController);
 app.use("/recipes", RecipeController);
 app.use("/words", WordController);
 app.use("/sessions", SessionController);
-
+app.use("/statistics", StatisticController);
 
 // take care of 404 errors
 // matches all routes
