f15f4804d51a35243b3fae08c4f68ad8656576be
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-05 10:43:05 +0200

    [FEATURE] add ingredient controller

---
+++ src/controllers/IngredientController.ts
@@ -0,0 +1,119 @@
+import express from "express";
+import { Request, Response } from "express";
+import { RecipeCompositeFacade } from "../db/composite/RecipeCompositeFacade";
+import {
+  HttpResponse,
+  HttpResponseStatus,
+  HttpResponseMessage,
+  HttpResponseMessageSeverity
+} from "../lib/utils/http/HttpResponse";
+import { logEndpoint } from "../util/log/endpointLogger";
+import { check } from "express-validator";
+import { retrieveValidationMessage } from "../util/validation/validationMessages";
+import {
+  checkRouteValidation,
+  failedValidation400Response
+} from "../util/validation/validationHelper";
+import { http4xxResponse } from "../util/http/httpResponses";
+import { IngredientFacade } from "../db/entity/kitchen/IngredientFacade";
+
+const router = express.Router();
+
+const controllerName = "IngredientController";
+
+/**
+ * GET /
+ *
+ * Get all ingredients.
+ *
+ * response:
+ * - ingredients: all ingredients of the application
+ */
+router.get("/", async (req: Request, res: Response, next: any) => {
+  const ingredientFacade = new IngredientFacade();
+  try {
+    const ingredients = await ingredientFacade.get();
+
+    logEndpoint(controllerName, `Return all ingredients!`, req);
+
+    return res
+      .status(200)
+      .json(
+        new HttpResponse(HttpResponseStatus.SUCCESS, ingredients, [
+          new HttpResponseMessage(
+            HttpResponseMessageSeverity.SUCCESS,
+            `Alle Zutate erfolgreich geladen!`
+          )
+        ])
+      );
+  } catch (e) {
+    return next(e);
+  }
+});
+
+/**
+ * GET /:id
+ *
+ * Get a ingredient by id.
+ *
+ * params:
+ * - id: id of the ingredient
+ *
+ * response:
+ * - ingredient: ingredient that was loaded
+ */
+router.get(
+  "/:id",
+  [
+    check("id")
+      .isNumeric()
+      .withMessage(retrieveValidationMessage("id", "numeric"))
+  ],
+  async (req: Request, res: Response, next: any) => {
+    if (!checkRouteValidation(controllerName, req, res)) {
+      return failedValidation400Response(req, res);
+    }
+
+    const id = Number(req.params.id);
+    const ingredientFacade = new IngredientFacade();
+    try {
+      const ingredient = await ingredientFacade.getById(id);
+
+      if (!ingredient) {
+        logEndpoint(
+          controllerName,
+          `Ingredient with id ${id} was not found!`,
+          req
+        );
+
+        return http4xxResponse(res, [
+          new HttpResponseMessage(
+            HttpResponseMessageSeverity.DANGER,
+            `Die Zutat konnte nicht gefunden werden.`
+          )
+        ]);
+      }
+
+      logEndpoint(
+        controllerName,
+        `Ingredient with id ${id} was successfully loaded!`,
+        req
+      );
+
+      return res
+        .status(200)
+        .json(
+          new HttpResponse(HttpResponseStatus.SUCCESS, ingredient, [
+            new HttpResponseMessage(
+              HttpResponseMessageSeverity.SUCCESS,
+              `Die Zutat wurde erfolgreich geladen!`
+            )
+          ])
+        );
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+export default router;
