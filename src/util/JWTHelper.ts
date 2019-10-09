574d28f38c60b13f6f87c874f233d53e22a5c1f5
Florian Mold <florian.mold@live.at>
2019-10-09 09:02:41 +0200

    [TASK] remove deprecated methods

--- src/util/JWTHelper.ts
+++ src/util/JWTHelper.ts
@@ -16,35 +16,6 @@ export class JWTHelper {
     private _expiresIn: number = 3600; // expires in 1 hour
 
     /**
-     * @deprecated
-     * signs the jwt token
-     * @param data data to sing
-     */
-    public async signToken(data: any) {
-        const therapistFacade = new TherapistFacade();
-        const isTherapist = await therapistFacade.isTherapist(data.id);
-        return jwt.sign(
-            { id: data.id, email: data.email, therapist: isTherapist},
-            this._secretKey,
-            {
-                expiresIn: this._expiresIn
-            }
-        );
-    }
-
-    /**
-     * @deprecated
-     * validates token}
-     * @param token token to verify
-     * @param cb callback to get the decoded token on
-     */
-    public async verifyToken(token: string, cb: jwt.VerifyCallback) {
-        return jwt.verify(token, this._secretKey, async function(err, decoded) {
-            return cb(err, decoded);
-        });
-    }
-
-    /**
      * creates jwt token for user
      *
      * @param user
