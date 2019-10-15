4a8d0b43154b9dda3ca484774658036f09b3a7c6
Florian Mold <florian.mold@live.at>
2019-10-15 21:23:44 +0200

    remove home test

--- test/home.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../src/app";
-
-describe("GET /home", () => {
-  it("should return 200 OK", (done) => {
-    request(app).get("/home")
-      .expect(200, done);
-  });
-});
