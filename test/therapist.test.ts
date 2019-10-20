337e581ac45709f9ee373a63d63b38c55161bc30
Florian Mold <florian.mold@live.at>
2019-10-20 11:27:53 +0200

    [TASK] therapists tests

--- test/therapist.test.ts
+++ test/therapist.test.ts
@@ -1,12 +1,12 @@
 import request from "supertest";
 import app from "../src/app";
 import { dropTables, runMigrations, seedTables, truncateTables } from "../src/migrationHelper";
-import { containsMessage } from "../src/util/testhelper";
+import { authenticate, containsMessage } from "../src/util/testhelper";
 import { HttpResponseMessageSeverity } from "../src/lib/utils/http/HttpResponse";
 import { TherapistFacade } from "../src/db/entity/user/TherapistFacade";
 import * as bcrypt from "bcryptjs";
 import { Status } from "../src/lib/enums/Status";
-import { validTherapist } from "../src/seeds/users";
+import { validPatient, validTherapist } from "../src/seeds/users";
 
 describe("POST /therapists", () => {
     const timeout = 100000;
@@ -438,5 +438,88 @@ describe("POST /therapists", () => {
         expect(res1.body._status).toEqual("fail");
         expect(containsMessage(res1.body._messages, HttpResponseMessageSeverity.DANGER, 6)).toBeTruthy();
 
+    }, timeout);
+});
+
+describe("GET /therapists", () => {
+    const endpoint = "/therapists";
+    const timeout = 10000;
+    let authenticationToken: string;
+
+    // drop tables
+    beforeAll(async () => {
+        return dropTables();
+    });
+
+    // run migrations
+    beforeAll(async () => {
+        return runMigrations();
+    });
+
+    // truncate tables
+    beforeEach(async () => {
+        return truncateTables();
+    });
+
+    // seed tables
+    beforeEach(async () => {
+        return seedTables();
+    });
+
+    it("fetch all therapists", async () => {
+        authenticationToken = await authenticate(validTherapist);
+
+        const res = await request(app).get(endpoint)
+            .set("Authorization", "Bearer " + authenticationToken)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200);
+
+        expect(res.body._status).toEqual("success");
+        expect(res.body._data).toHaveProperty("token");
+        expect(res.body._data).toHaveProperty("therapists");
+        expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.SUCCESS, 1)).toBeTruthy();
+    }, timeout);
+
+    it("try to fetch all therapists without authentication", async () => {
+        const res = await request(app).get(endpoint)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(401);
+
+        const res1 = await request(app).get(endpoint)
+            .set("Authorization", "")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(401);
+
+        expect(res.body._status).toEqual("fail");
+        expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+    }, timeout);
+
+    it("try to fetch all therapists with expired token", async () => {
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJwYXRpZW50QGV4YW1wbGUub3JnIiwidGhlcmFwaXN0IjpmYWxzZSwiaWF0IjoxNTcxNTE4OTM2LCJleHAiOjE1NzE1MTg5Mzd9.7cZxI_6qvVSL3xhSl0q54vc9QH7JPB_E1OyrAuk1eiI";
+
+        const res = await request(app).get(endpoint)
+            .set("Authorization", "Bearer " + token)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(401);
+
+        expect(res.body._status).toEqual("fail");
+        expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+    }, timeout);
+
+    it("try to fetch all therapists with patient user", async () => {
+        authenticationToken = await authenticate(validPatient);
+
+        const res = await request(app).get(endpoint)
+            .set("Authorization", "Bearer " + authenticationToken)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(403);
+
+        expect(res.body._status).toEqual("fail");
+        expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
     }, timeout);
 });
