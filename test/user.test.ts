f3b5ebcbdab7b238ad04e044330cd7fde46e7082
Florian Mold <florian.mold@live.at>
2019-10-24 09:26:23 +0200

    [TASK] GET /users/related tests

---
+++ test/user.test.ts
@@ -0,0 +1,72 @@
+import request from "supertest";
+import app from "../src/app";
+import { dropTables, runMigrations, seedTables, truncateTables } from "../src/migrationHelper";
+import { authenticate, containsMessage } from "../src/util/testhelper";
+import { validTherapist } from "../src/seeds/users";
+import { HttpResponseMessageSeverity } from "../src/lib/utils/http/HttpResponse";
+
+describe("UserController Tests", () => {
+
+    describe("GET /users/related", () => {
+        const endpoint = "/users/related";
+        const timeout = 10000;
+        let authenticationToken: string;
+
+        beforeAll(async () => {
+            await dropTables();
+            await runMigrations();
+            await truncateTables();
+            await seedTables();
+        }, timeout);
+
+        it("retrieve the user for the jwt", async () => {
+            authenticationToken = await authenticate(validTherapist);
+
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + authenticationToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200);
+
+            expect(res.body._status).toEqual("success");
+            expect(res.body._data).toHaveProperty("token");
+            expect(res.body._data).toHaveProperty("user");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.SUCCESS, 1)).toBeTruthy();
+            expect(res.body._data.user._id).toEqual(validTherapist.id);
+
+        }, timeout);
+
+        it("trying to receive the user with jwt without authentication", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+
+            const res1 = await request(app).get(endpoint)
+                .set("Authorization", "")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res1.body._status).toEqual("fail");
+            expect(containsMessage(res1.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+
+        }, timeout);
+
+        it("try to fetch user with jwt with an expired token", async () => {
+            const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJwYXRpZW50QGV4YW1wbGUub3JnIiwidGhlcmFwaXN0IjpmYWxzZSwiaWF0IjoxNTcxNTE4OTM2LCJleHAiOjE1NzE1MTg5Mzd9.7cZxI_6qvVSL3xhSl0q54vc9QH7JPB_E1OyrAuk1eiI";
+
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + token)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+    });
+});
