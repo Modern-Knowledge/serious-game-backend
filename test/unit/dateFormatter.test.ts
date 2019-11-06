b2705cda77fa7d1b535932df75cc566a25014f8a
Florian Mold <florian.mold@live.at>
2019-11-06 14:19:19 +0100

    [TASK] dateFormatter tests

---
+++ test/unit/dateFormatter.test.ts
@@ -0,0 +1,30 @@
+import { formatDate, formatDateTime, formatTime } from "../../src/lib/utils/dateFormatter";
+import moment from "moment";
+
+describe("lib/utils/dateFormatter Tests", () => {
+
+    beforeAll(async () => {
+        moment.locale("de");
+    });
+
+    it("check date time formatter!", async () => {
+        const date = new Date(2019, 0, 1, 12, 0, 0);
+        const formattedDate = formatDateTime(date);
+
+        expect(formattedDate).toEqual("01.01.2019 12:00");
+    });
+
+    it("check date formatter!", async () => {
+        const date = new Date(2019, 0, 1);
+        const formattedDate = formatDate(date);
+
+        expect(formattedDate).toEqual("01.01.2019");
+    });
+
+    it("check time formatter!", async () => {
+        const date = new Date(2019, 0, 1, 12, 0, 0);
+        const formattedDate = formatTime(date);
+
+        expect(formattedDate).toEqual("12:00");
+    });
+});
