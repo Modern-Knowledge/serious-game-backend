1fafe6cb71103eecac32f0fa3f892e274425f728
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-11-11 00:38:51 +0100

    [FEATURE] insert error texts

--- src/controllers/ErrortextController.ts
+++ src/controllers/ErrortextController.ts
@@ -1,19 +1,24 @@
+import express, { Request, Response } from 'express';
+import { check } from 'express-validator';
 
-
-import express from "express";
-import { Request, Response } from "express";
-import { HttpResponse, HttpResponseStatus, HttpResponseMessage, HttpResponseMessageSeverity } from "../lib/utils/http/HttpResponse";
-import { logEndpoint } from "../util/log/endpointLogger";
-import { check } from "express-validator";
-import { rVM } from "../util/validation/validationMessages";
-import { checkRouteValidation } from "../util/validation/validationHelper";
-import { failedValidation400Response, http4xxResponse } from "../util/http/httpResponses";
-import { ErrortextFacade } from "../db/entity/helptext/ErrortextFacade";
-import { checkAuthentication, checkAuthenticationToken } from "../util/middleware/authenticationMiddleware";
+import { ErrortextFacade } from '../db/entity/helptext/ErrortextFacade';
+import { ErrortextStatisticFacade } from '../db/entity/helptext/ErrortextStatisticFacade';
+import { ErrortextStatistic } from '../lib/models/ErrortextStatistic';
+import {
+  HttpResponse,
+  HttpResponseMessage,
+  HttpResponseMessageSeverity,
+  HttpResponseStatus,
+} from '../lib/utils/http/HttpResponse';
+import { failedValidation400Response, http4xxResponse } from '../util/http/httpResponses';
+import { logEndpoint } from '../util/log/endpointLogger';
+import { checkAuthentication, checkAuthenticationToken } from '../util/middleware/authenticationMiddleware';
+import { checkRouteValidation } from '../util/validation/validationHelper';
+import { rVM } from '../util/validation/validationMessages';
 
 const router = express.Router();
 
-const controllerName = "ErrortextController";
+const controllerName = 'ErrortextController';
 
 const authenticationMiddleware = [checkAuthenticationToken, checkAuthentication];
 
@@ -26,22 +31,31 @@ const authenticationMiddleware = [checkAuthenticationToken, checkAuthentication]
  * - errortexts: all errortexts of the application
  * - token: authentication token
  */
-router.get("/", authenticationMiddleware, async (req: Request, res: Response, next: any) => {
-    const errorTextFacade = new ErrortextFacade();
+router.get('/', authenticationMiddleware, async (req: Request, res: Response, next: any) => {
+  const errorTextFacade = new ErrortextFacade();
 
-    try {
-        const errortexts = await errorTextFacade.get();
+  try {
+    const errortexts = await errorTextFacade.get();
 
-        logEndpoint(controllerName, `Return all errortexts!`, req);
+    logEndpoint(controllerName, `Return all errortexts!`, req);
 
-        return res.status(200).json(new HttpResponse(HttpResponseStatus.SUCCESS,
-            {errortexts: errortexts, token: res.locals.authorizationToken}, [
-                new HttpResponseMessage(HttpResponseMessageSeverity.SUCCESS, `Alle Fehlertexte erfolgreich geladen!`)
-            ]
-        ));
-    } catch (e) {
-        return next(e);
-    }
+    return res
+      .status(200)
+      .json(
+        new HttpResponse(
+          HttpResponseStatus.SUCCESS,
+          { errortexts: errortexts, token: res.locals.authorizationToken },
+          [
+            new HttpResponseMessage(
+              HttpResponseMessageSeverity.SUCCESS,
+              `Alle Fehlertexte erfolgreich geladen!`
+            )
+          ]
+        )
+      );
+  } catch (e) {
+    return next(e);
+  }
 });
 
 /**
@@ -56,38 +70,177 @@ router.get("/", authenticationMiddleware, async (req: Request, res: Response, ne
  * - errortext: loaded errortext
  * - token: authentication token
  */
-router.get("/:id", authenticationMiddleware, [
-    check("id").isNumeric().withMessage(rVM("id", "numeric"))
-], async (req: Request, res: Response, next: any) => {
-
+router.get(
+  '/:id',
+  authenticationMiddleware,
+  [
+    check('id')
+      .isNumeric()
+      .withMessage(rVM('id', 'numeric'))
+  ],
+  async (req: Request, res: Response, next: any) => {
     if (!checkRouteValidation(controllerName, req, res)) {
-        return failedValidation400Response(req, res);
+      return failedValidation400Response(req, res);
     }
 
     const id = Number(req.params.id);
     const errortextFacade = new ErrortextFacade();
 
     try {
-        const errortext = await errortextFacade.getById(id);
+      const errortext = await errortextFacade.getById(id);
 
-        if (!errortext) {
-            logEndpoint(controllerName, `Errortext with id ${id} not found!`, req);
+      if (!errortext) {
+        logEndpoint(controllerName, `Errortext with id ${id} not found!`, req);
 
-            return http4xxResponse(res, [
-                new HttpResponseMessage(HttpResponseMessageSeverity.DANGER, `Der Fehlertext wurde nicht gefunden!`)
-            ]);
-        }
+        return http4xxResponse(res, [
+          new HttpResponseMessage(
+            HttpResponseMessageSeverity.DANGER,
+            `Der Fehlertext wurde nicht gefunden!`
+          )
+        ]);
+      }
 
-        logEndpoint(controllerName, `Errortext with id ${id} was successfully loaded!`, req);
+      logEndpoint(controllerName, `Errortext with id ${id} was successfully loaded!`, req);
 
-        return res.status(200).json(new HttpResponse(HttpResponseStatus.SUCCESS,
-            {errortext: errortext, token: res.locals.authorizationToken}, [
-                new HttpResponseMessage(HttpResponseMessageSeverity.SUCCESS, `Der Fehlertext wurde erfolgreich gefunden!`)
+      return res
+        .status(200)
+        .json(
+          new HttpResponse(
+            HttpResponseStatus.SUCCESS,
+            { errortext: errortext, token: res.locals.authorizationToken },
+            [
+              new HttpResponseMessage(
+                HttpResponseMessageSeverity.SUCCESS,
+                `Der Fehlertext wurde erfolgreich gefunden!`
+              )
             ]
-        ));
+          )
+        );
     } catch (e) {
-        return next(e);
+      return next(e);
     }
+  }
+);
+
+/**
+ * POST /
+ *
+ * Insert an errortext
+ *
+ *
+ * response:
+ * - errortext: the created errortext
+ * - token: authentication token
+ */
+router.post('/', authenticationMiddleware, async (req: Request, res: Response, next: any) => {
+  if (!checkRouteValidation(controllerName, req, res)) {
+    return failedValidation400Response(req, res);
+  }
+
+  const errortextData = req.body;
+  const errortextFacade = new ErrortextStatisticFacade();
+
+  try {
+    const errorTextStatistic = new ErrortextStatistic();
+    errorTextStatistic.errortextId = errortextData.errortext.id;
+    errorTextStatistic.statisticId = errortextData.session._statisticId;
+    const errortext = await errortextFacade.insertErrortextStatistic(errorTextStatistic);
+
+    if (!errortext) {
+      return http4xxResponse(res, [
+        new HttpResponseMessage(
+          HttpResponseMessageSeverity.DANGER,
+          `Fehler beim erstellen des Fehlertexts!`
+        )
+      ]);
+    }
+
+    logEndpoint(controllerName, `Errortext was successfully created!`, req);
+
+    return res
+      .status(200)
+      .json(
+        new HttpResponse(
+          HttpResponseStatus.SUCCESS,
+          { errortext: errortext, token: res.locals.authorizationToken },
+          [
+            new HttpResponseMessage(
+              HttpResponseMessageSeverity.SUCCESS,
+              `Der Fehlertext wurde erfolgreich erstellt!`
+            )
+          ]
+        )
+      );
+  } catch (e) {
+    return next(e);
+  }
+});
+
+/**
+ * POST /bulk
+ *
+ * Insert multiple errortexts at once
+ *
+ *
+ * response:
+ * - errortexts: the created errortexts
+ * - token: authentication token
+ */
+router.post('/bulk', authenticationMiddleware, async (req: Request, res: Response, next: any) => {
+  if (!checkRouteValidation(controllerName, req, res)) {
+    return failedValidation400Response(req, res);
+  }
+
+  /**
+   * FIXME:
+   * remove duplicate errortexts, as inserting duplicate errortexts will cause an error, because the primary key
+   * will not be unique
+   */
+  const errortextsData: any[] = req.body.errortexts
+    .map(errortext => errortext['_id'])
+    .map((errortext: any, index: any, final: any) => final.indexOf(errortext) === index && index)
+    .filter(errortext => req.body.errortexts[errortext])
+    .map(errortext => req.body.errortexts[errortext]);
+
+  const errortextFacade = new ErrortextStatisticFacade();
+
+  try {
+    const errorTextStatistic = new ErrortextStatistic();
+    const errortexts = [];
+    for (const errortextData of errortextsData) {
+      errorTextStatistic.errortextId = errortextData._id;
+      errorTextStatistic.statisticId = req.body.session._statisticId;
+      errortexts.push(await errortextFacade.insertErrortextStatistic(errorTextStatistic));
+    }
+
+    if (!errortexts) {
+      return http4xxResponse(res, [
+        new HttpResponseMessage(
+          HttpResponseMessageSeverity.DANGER,
+          `Fehler beim Erstellen der Fehlertexte!`
+        )
+      ]);
+    }
+
+    logEndpoint(controllerName, `Errortexts were successfully created!`, req);
+
+    return res
+      .status(200)
+      .json(
+        new HttpResponse(
+          HttpResponseStatus.SUCCESS,
+          { errortexts: errortexts, token: res.locals.authorizationToken },
+          [
+            new HttpResponseMessage(
+              HttpResponseMessageSeverity.SUCCESS,
+              `Die Fehlertexte wurden erfolgreich erstellt!`
+            )
+          ]
+        )
+      );
+  } catch (e) {
+    return next(e);
+  }
 });
 
 export default router;
