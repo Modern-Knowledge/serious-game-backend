e8f9f5b610d3bfd5366182e660b2bf4091f20de2
Florian Mold <florian.mold@live.at>
2019-11-27 00:00:37 +0100

    [TASK] update build script

--- migrations/013.create-images.sql
+++ migrations/013.create-images.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `images`
 (
     `id`          INT(11)   NOT NULL AUTO_INCREMENT,
-    `image`       BLOB      NOT NULL,
+    `image`       LONGBLOB      NOT NULL,
     `created_at`  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `modified_at` TIMESTAMP NULL     DEFAULT NULL,
     PRIMARY KEY (`id`)
