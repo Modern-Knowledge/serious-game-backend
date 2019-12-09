a2623fc33dabdb779cbd28f8934efb21165f3fb4
Florian Mold <florian.mold@live.at>
2019-12-09 10:43:19 +0100

    document code

--- src/util/permission/permissionGuard.ts
+++ src/util/permission/permissionGuard.ts
@@ -4,15 +4,13 @@ import { loggerString } from "../Helper";
 import logger from "../log/logger";
 
 /**
- * functions to check permissions in routes
- */
-
-/**
- * checks if the authUser is allowed to view/edit the specified resources
- * retrieves the user id from the resource and compare it the the auth user
+ * Checks if the auth-user is allowed to view/edit the specified resources that are loaded by the endpoint.
+ * Checks if the resources belong to the user, by checking the user-id from the resource. Afterwards the id of the
+ * auth-user is compared with the id of the resource. If the user is allowed to view all resource, the function returns
+ * true. Otherwise false is returned.
  *
- * @param authUser authUser to validate permission
- * @param resources resource to check
+ * @param authUser auth-user where the permission should be validated
+ * @param resources array of resources that should be checked
  */
 export function validatePermission(authUser: User, resources: IUserInterface[]): boolean {
     logger.debug(`${loggerString(__dirname, "permissionGuard", "validatePermission")}`);
