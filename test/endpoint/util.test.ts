f334234e1c2d4d2937c8500dcaecd9879b7c0718
Florian Mold <florian.mold@live.at>
2020-02-16 13:42:43 +0100

    [TASK] Add tests to util-controller

---
+++ test/endpoint/util.test.ts
@@ -0,0 +1,151 @@
+import request from "supertest";
+import app from "../../src/app";
+import {HttpResponseMessageSeverity} from "../../src/lib/utils/http/HttpResponse";
+import {validAdminTherapist} from "../../src/seeds/users";
+import {authenticate, containsMessage} from "../../src/util/testhelper";
+
+const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Niwi" +
+    "ZW1haWwiOiJwYXRpZW50QGV4YW1wbGUub3JnIiwidGhlcmFwaXN0IjpmYWx" +
+    "zZSwiaWF0IjoxNTcxNTE4OTM2LCJleHAiOjE1NzE1MTg5Mzd9.7cZxI_6qv" +
+    "VSL3xhSl0q54vc9QH7JPB_E1OyrAuk1eiI";
+
+describe("UtilController Tests", () => {
+
+    describe("GET /util/mail-server", () => {
+        const endpoint = "/util/mail-server";
+        const timeout = 10000;
+        let authenticationToken: string;
+
+        // SGBUCA01
+        it("check if mail-server is reachable", async () => {
+            authenticationToken = await authenticate(validAdminTherapist);
+
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + authenticationToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200);
+
+            expect(res.body._status).toEqual("success");
+            expect(res.body._data).toHaveProperty("connectable");
+
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.SUCCESS, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA02
+        it("try to check mail-server is reachable without authentication", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA03
+        it("try to check mail-server is reachable with an expired token", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + expiredToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+    });
+
+    describe("GET /util/database", () => {
+        const endpoint = "/util/database";
+        const timeout = 10000;
+        let authenticationToken: string;
+
+        // SGBUCA04
+        it("fetch if database is reachable", async () => {
+            authenticationToken = await authenticate(validAdminTherapist);
+
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + authenticationToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200);
+
+            expect(res.body._status).toEqual("success");
+            expect(res.body).toHaveProperty("_data");
+            expect(res.body._data).toHaveProperty("connectable");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.SUCCESS, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA05
+        it("try to fetch database-connectivity without authentication", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA06
+        it("try to fetch database-connectivity with an expired token", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + expiredToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+    });
+
+    describe("GET /util/database-version", () => {
+        const endpoint = "/util/database-version";
+        const timeout = 10000;
+        let authenticationToken: string;
+
+        // SGBUCA07
+        it("fetch database-version", async () => {
+            authenticationToken = await authenticate(validAdminTherapist);
+
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + authenticationToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200);
+
+            expect(res.body._status).toEqual("success");
+            expect(res.body).toHaveProperty("_data");
+            expect(res.body._data).toHaveProperty("version");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.SUCCESS, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA08
+        it("try to fetch database-version without authentication", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+        // SGBUCA09
+        it("try to fetch database-version with an expired token", async () => {
+            const res = await request(app).get(endpoint)
+                .set("Authorization", "Bearer " + expiredToken)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401);
+
+            expect(res.body._status).toEqual("fail");
+            expect(containsMessage(res.body._messages, HttpResponseMessageSeverity.DANGER, 1)).toBeTruthy();
+        }, timeout);
+
+    });
+});
